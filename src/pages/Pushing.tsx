e02455e39eff57a90438816f4ce9610953d6d5e6
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-09-06 22:48:16 +0100

    src/pages/Pushing.tsx

--- src/pages/Pushing.tsx
+++ src/pages/Pushing.tsx
@@ -17,23 +17,46 @@ const MainContent = () => {
         <div>
           <Navbar />
           <h1 className="text-[34px] lg:text-[55px] font-black font-averta text-[#444a52]">
-            Pricing
+            Pushing Chefs
           </h1>
           <p className="mt-4 text-[17px] lg:text-[19px] font-averta text-[#444a52]">
-            We know you make a lot of amazing food contents on your social
-            platforms like Instagram, TikTok, and others. We also know that most
+            We know you make a lot of amazing food content on your social
+            platforms like instagram, tiktok and others. We also know that most
             times, the best reward you get from the contents you worked so hard
-            to make are likes and comments, with very few people actually coming
-            to your DMs to buy.
+            to make are “likes and comments” with a very small amount of people
+            actually coming to your DMs to buy.
           </p>
           <p className="mt-4 text-[17px] lg:text-[19px] font-averta text-[#444a52]">
-            Ne tum quidem te respicies et cogitabis sibi quemque natum esse et
-            suis voluptatibus? Et nunc quidem quod eam tuetur, ut de vite
-            potissimum loquar, est id extrinsecus.
+            With Pusheat, you can now sell the content(s) of your posts with
+            ease. Our platform transforms your recipes, food items, groceries
+            and food creations into irresistible deals for your followers.
           </p>
-          <p className="mt-4 text-[17px] lg:text-[19px] font-averta text-[#444a52]">
-            Videamus igitur sententias eorum?
+          <p className="mt-16 text-[17px] lg:text-[19px] decoration-[#01AA1B] underline decoration-wavy  font-averta text-[#444a52]">
+            Here are 3 reasons you’ll love Pusheat.
           </p>
+
+          <ul className="mt-4 text-[17px] lg:text-[19px] font-averta text-[#444a52]">
+            <li className="list-disc list-inside">
+              <b> A normal order on Pusheat is a bulk order:</b> Every order on
+              Pusheat is a bulk order, guaranteeing at least two customers
+              before you cook. This ensures optimal efficiency and helps you
+              maximize your profits. Single orders are also available on special
+              requests.
+            </li>
+            <li className="list-disc list-inside">
+              <b> Get paid before you cook:</b> The risk of cooking and waiting
+              for customers can be very high. With Pusheat, you receive full
+              payment from your customers before you even start cooking. Enjoy
+              peace of mind and focus on creating delicious meals.
+            </li>
+            <li className="list-disc list-inside">
+              <b>Cook and sell on your terms:</b> Say goodbye to the stress of
+              managing inventory, kitchen/store hours, and catalog availability.
+              With Pusheat, you're in complete control. Post and cook when it
+              suits you, and sell your culinary creations or products at your
+              own pace.
+            </li>
+          </ul>
         </div>
         <div className="mt-auto">
           <hr className="border-gray-600 my-10" />
