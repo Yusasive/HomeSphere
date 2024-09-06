45be1ea30607f0cdd5ac2f8568bbdd263f3f3e79
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-09-06 22:48:48 +0100

    src/pages/Pushing.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "icons-pattern": `url('/src/assets/icons/icons.svg'), url('/src/assets/icons/icons1.svg'), url('/src/assets/icons/icons2.svg'), url('/src/assets/icons/icons3.svg'),
+                          url('/src/assets/icons/icons4.svg'), url('/src/assets/icons/icons5.svg'), url('/src/assets/icons/icons6.svg'), url('/src/assets/icons/icons7.svg'),
+                          url('/src/assets/icons/icons8.svg'), url('/src/assets/icons/icons9.svg'), url('/src/assets/icons/icons10.svg')`,
+      },
       fontFamily: {
-        nexa: 'Nexa',
-        averta: 'Averta', // Add your custom font here
+        nexa: "Nexa",
+        averta: "Averta", // Add your custom font here
       },
     },
   },
